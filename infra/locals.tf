9c88819a2684bf606dbf72196770355eff2b64a7
Jairo <hardtelles@gmail.com>
2023-10-31 02:53:47 -0300

    remove locals

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,7 +1,4 @@
 locals {
-  lambdas_path = "${path.module}/lambdas"
-  #layers_path  = "${path.module}/layers"
-
   common_tags = {
     Project   = "Lambda Authorizer FIAP 56 with Terraform"
     CreatedAt = formatdate("YYYY-MM-DD", timestamp())

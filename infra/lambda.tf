a93ce736cd3cbc54ee34d7f09c6c38e0ec92e7bc
Jairo <hardtelles@gmail.com>
2024-03-08 07:33:40 -0300

    fase 5 init

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -6,9 +6,8 @@ resource "aws_lambda_function" "lambda_authorizer" {
   function_name = var.lambda_function_name
   handler       = var.lambda_handler
   description   = var.description
-  role          = aws_iam_role.lambda_authorizer_lambda.arn
+  role          = var.lab_role_arn #aws_iam_role.lambda_sts_lambda.arn (adicionado devedo restricoes lab aws academy)
   runtime       = var.lambda_runtime
-
   filename         = local.lambda_payload_filename
   source_code_hash = base64sha256(filebase64(local.lambda_payload_filename))
 
@@ -17,8 +16,17 @@ resource "aws_lambda_function" "lambda_authorizer" {
 
   environment {
     variables = {
-      USER_POOL_ID : var.user_pool_id
+      USER_POOL_ID : jsondecode(data.aws_secretsmanager_secret_version.credentials_sts.secret_string)["userPoolId"]
     }
   }
 
+}
+
+#obteando dados do secret manager STS
+data "aws_secretsmanager_secret" "sts" {
+  name = "prod/soat1grupo56/Sts"
+}
+
+data "aws_secretsmanager_secret_version" "credentials_sts" {
+  secret_id = data.aws_secretsmanager_secret.sts.id
 }
